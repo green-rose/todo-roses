41fcd90a094b95a6269260ac0b0d267496b45ca5
Mila Karous <m.k.rose@seznam.cz>
2020-07-22 13:11:01 +0200

    init

---
+++ src/main/java/com/gfa/todoroses/TodoRosesApplication.java
@@ -0,0 +1,13 @@
+package com.gfa.todoroses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoRosesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoRosesApplication.class, args);
+    }
+
+}
